3bb321f8987dd5065851fd716100b0672ae9d42c
alejof16 <112113402+alejof16@users.noreply.github.com>
2022-10-21 16:46:38 -0500

    Delete ejercicio2.py

--- ejercicio2.py
+++
@@ -1,9 +0,0 @@
-# EJERCICIONUMERO 2
-
-numeros = [24,150,300,670,295,1050,44,33]
-for x in numeros:
-    if x <=600:
-            if x % 5 == 0:
-              if x <=1000 :
-                  print (x)
-             
